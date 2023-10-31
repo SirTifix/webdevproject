212ccfe347bc12569d724da3dc58f0f988c25ae9
SirTifix <saludoraf@gmail.com>
2023-11-01 00:37:14 +0800

    debug

--- functions/edit.php
+++ functions/edit.php
@@ -3,8 +3,8 @@
 include_once("../dbConnection/mysqlconfig_connection.php");
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
-    $code = $_POST['subject_code'];
-    $name = $_POST['subject_name'];
+    $code = $_POST['code'];
+    $name = $_POST['name'];
     // checking empty fields
     if (empty($code) || empty($name)) {
         if (empty($code)) {
