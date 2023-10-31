eee1b6ef363549d453e607f60fa814e8e71ef3cf
SirTifix <saludoraf@gmail.com>
2023-11-01 00:26:37 +0800

    rechanged other codes that i have debugged on my notepad copy

--- functions/add.php
+++ functions/add.php
@@ -8,30 +8,28 @@
 //including the database connection file
 include_once("../dbConnection/mysqlconfig_connection.php");
 
-if(isset($_POST["Submit"])){
-    $code = $_POST ["code"];
-    $name = $_POST ["name"];
-    // checking fields
+if (isset($_POST['Submit'])) {
+    $code = $_POST['subject_code'];
+    $name = $_POST['subject_name'];
+    //checking empty fields
     if (empty($code) || empty($name)) {
         if (empty($code)) {
-            echo "<font color=\"red\">Subject Code field is empty.</font><br/>";
+            echo '<font color="red">Subjet Code field is empty.</font><br/>';
         }
         if (empty($name)) {
-            echo "<font color=\"red\">Name field is empty.</font><br/>";
+            echo '<font color="red">Subjet Name field is empty.</font><br/>';
         }
-        echo "<br> <a  href='javascript:self.history.back();'> Go Back</a>";
-    }
-    else{
-        //if all fields are filled (not empty)
-        //insert data to database
+        //link to the previous page
+        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+    } else {
+        //if all the fields are filled
+        //insert data into the database
         $result = mysqli_query($dbc, "INSERT INTO tblsubjects(subject_code, subject_name) VALUES('$code','$name')");
         //display success message
-        echo "<font color='green'> Data added successfully.";
-        echo "<br/><a href='../functions/index.php>View Result </a>";
+        echo "<font color='green'>Data added successfully.</font>";
+        echo '<br/> <a href="../index.php">View Result</a>';
     }
-    
 }
-
 ?>
  </body>
 </html>
--- functions/delete.php
+++ functions/delete.php
@@ -1,10 +1,6 @@
 <?php
-    //including the database connection file
     include_once("../dbConnection/mysqlconfig_connection.php");
-    //getting the id of data from url
     $id = $_GET['id'];
-    //deleting the row from table
     mysqli_query($dbc, "DELETE FROM tblsubjects WHERE subject_id='$id'");
-    //redirecting to the display page (index.php in our case)
-    header("Location: ../functions/index.php");
+    header("Location: ../index.php");
 ?>
