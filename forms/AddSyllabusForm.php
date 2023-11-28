4ea786a626d7008440c7a96165ac44f1c689f3da
SirTifix <saludoraf@gmail.com>
2023-11-28 22:49:17 +0800

    Update AddSyllabusForm.php

--- forms/AddSyllabusForm.php
+++ forms/AddSyllabusForm.php
@@ -3,5 +3,9 @@
     <head>
         <title>Add Syllabus</title>
     </head>
-
+    <body>
+        <h1>Add Syllabus</h1>
+        <a href="../index.php">Home</a>
+        <br /><br />
+    </body>
 </html>
