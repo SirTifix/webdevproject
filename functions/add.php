7abe0b70c695ff1405fb71c2279fe08d057ea1e5
SirTifix <saludoraf@gmail.com>
2023-11-28 22:57:10 +0800

    functions`

--- functions/add.php
+++ functions/add.php
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> Add Data </title>
-    </head>
+<head>
+    <title>Add Data</title>
+</head>
     <body>
         <?php
-//including the database connection file
-include_once("../dbConnection/mysqlconfig_connection.php");
+            include_once("../dbConnection/mysqlconfig_connection.php");
 
-if (isset($_POST['Submit'])) {
-    $code = $_POST['code'];
-    $name = $_POST['name'];
-    //checking empty fields
-    if (empty($code) || empty($name)) {
-        if (empty($code)) {
-            echo '<font color="red">Subjet Code field is empty.</font><br/>';
-        }
-        if (empty($name)) {
-            echo '<font color="red">Subjet Name field is empty.</font><br/>';
-        }
-        //link to the previous page
-        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
-    } else {
-        //if all the fields are filled
-        //insert data into the database
-        $result = mysqli_query($dbc, "INSERT INTO tblsubjects(subject_code, subject_name) VALUES('$code','$name')");
-        //display success message
-        echo "<font color='green'>Data added successfully.</font>";
-        echo '<br/> <a href="../index.php">View Result</a>';
-    }
-}
-?>
- </body>
+            if(isset($_POST['Submit'])) {
+                $code = $_POST['code'];
+                $name = $_POST['name'];
+
+                if(empty($code) || empty($name)) {
+                    if(empty($code)) {
+                        echo "<p style='color: red; font-size: 1.3em;'>Subject code field is empty.</p>";
+                    }
+                    if(empty($name)) {
+                        echo "<p style='color: red; font-size: 1.3em;'>Subject name field is empty.</p>";
+                    }
+
+                    echo "<a href='javascript:self.history.back();' style='color: blue; font-size: 1.3em;'>Go Back</a>";
+                }
+                else {
+                    $result = mysqli_query($dbc, "INSERT INTO tblsubjects(Subject_Code, Subject_Name) VALUES('$code','$name')");
+
+                    echo "<p style='color: green; font-size: 1.5em;'>Data added successfully!</p>";
+                    echo "<a href='../index.php' style='color: blue; font-size: 1.3em;'>View Results</a>";
+                }
+            }
+        ?>
+    </body>
 </html>
