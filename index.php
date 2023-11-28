b168f5303d1850552adb1c4a8dd61222ad97a2bb
SirTifix <saludoraf@gmail.com>
2023-11-28 23:04:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,43 @@
 <?php
-    include_once("dbConnections/mysqlconfig_conn.php"); 
+    include_once("dbconnection/mysqlconfig_connection.php");
     include_once("functions/fetch.php");
 ?>
-
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Sample CRUD</title>
-    </head>
-    <body>
-        <h1>My Subjects</h1>
-        <a href="forms/addform.php">Add Subject</a><br />
-        <a href="forms/AddSyllabusForm.php">Add Syllabus</a><br /><br />
-        <table width='100%' border=1>
-            <tr bgcolor='#CCCCCC'>
-                <td>ID</td>
-                <td>Subject Code</td>
-                <td>Subject Name</td>
-                <td>Syllabus ID</td>
-                <td>Syllabus Code</td>
-                <td>Syllabus Author</td>
-                <td>Action</td>
-            </tr>
-            <?php
-                while ($res = mysqli_fetch_array($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $res["Subject_ID"] . "</td>";
-                    echo "<td>" . $res['Subject_Code'] . "</td>";
-                    echo "<td>" . $res['Subject_Name'] . "</td>";
-                    echo "<td>" . $res['syllabus_id'] . "</td>";
-                    echo "<td>" . $res['syllabus_code'] . "</td>";
-                    echo "<td>" . $res['syllabus_author'] . "</td>";
-                    echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
-                        <a href=\"functions/delete.php?id=$res[Subject_ID]\"
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample CRUD</title>
+</head>
+<body>
+    <h1 style="text-align: center;">MY SUBJECTS</h1>
+    <table width = '100%' border=1.5>
+    <tr style="text-align: center;" bgcolor='#CCCCCC' color='#000000'>
+        <td style="font-weight: bold;">ID</td>
+        <td style="font-weight: bold;">Subject Code</td>
+        <td style="font-weight: bold;">Subject Name</td>
+        <td style="font-weight: bold;">Syllabus ID</td>
+        <td style="font-weight: bold;">Syllabus Code</td>
+        <td style="font-weight: bold;">Syllabus Author</td>
+        <td style="font-weight: bold;" width="15%">Action</td>
+    </tr>
+
+        <?php
+            while($res = mysqli_fetch_array($result))   {
+                echo "<tr>";
+                echo "<td>".$res['Subject_ID']."</td>";
+                echo "<td>".$res['Subject_Code']."</td>";
+                echo "<td>".$res['Subject_Name']."</td>";
+                echo "<td>".$res['syllabus_id']."</td>";
+                echo "<td>".$res['syllabus_code']."</td>";
+                echo "<td>".$res['syllabus_author']."</td>";
+                echo "<td><a href=\"forms/editform.php?id=".$res['Subject_ID']."\">Edit</a> |
+                        <a href=\"functions/delete.php?id=".$res['Subject_ID']."\"
                         onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-                }
-            ?>
-        </table>
-    </body>
+            }
+        ?>
+    </table><br/>
+    <a href="forms/addform.php">ADD SUBJECT</a><br/>
+    <a href="forms/addsyllabusform.php">ADD SYLLABUS</a>
+</body>
 </html>
