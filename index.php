fb767ca51c55b2a939ac13d0733087bb2a88ebba
SirTifix <saludoraf@gmail.com>
2023-11-28 22:59:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,41 @@
 <?php
-//including the database connection file
-include_once('dbConnection/mysqlconfig_connection.php');
-//including the fetch file
-include_once('functions/fetch.php');
+    include_once("dbConnections/mysqlconfig_conn.php"); 
+    include_once("functions/fetch.php");
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample CRUD</title>
-</head>
-
-<body>
-    <h1>My Subjects</h1>
-    <a href="./forms/addform.php">Add Subject</a>
-    <table width='100%' border=1>
-        <tr bgcolor='#CCCCCC'>
-            <td>ID</td>
-            <td>Subject Code</td>
-            <td>Subject Name</td>
-            <td>Action</td>
-        </tr>
-        <?php
-        while ($res = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>" . $res['subject_id'] . "</td>";
-            echo "<td>" . $res['subject_code'] . "</td>";
-            echo "<td>" . $res['subject_name'] . "</td>";
-            echo "<td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
-                    <a href=\"Functions/delete.php?id=$res[subject_id]\"
-                    onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-        }
-        ?>
-    </table>
-</body>
-
+<html>
+    <head>
+        <title>Sample CRUD</title>
+    </head>
+    <body>
+        <h1>My Subjects</h1>
+        <a href="forms/addform.php">Add Subject</a><br />
+        <a href="forms/AddSyllabusForm.php">Add Syllabus</a><br /><br />
+        <table width='100%' border=1>
+            <tr bgcolor='#CCCCCC'>
+                <td>ID</td>
+                <td>Subject Code</td>
+                <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
+                <td>Action</td>
+            </tr>
+            <?php
+                while ($res = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $res["Subject_ID"] . "</td>";
+                    echo "<td>" . $res['Subject_Code'] . "</td>";
+                    echo "<td>" . $res['Subject_Name'] . "</td>";
+                    echo "<td>" . $res['syllabus_id'] . "</td>";
+                    echo "<td>" . $res['syllabus_code'] . "</td>";
+                    echo "<td>" . $res['syllabus_author'] . "</td>";
+                    echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
+                        <a href=\"functions/delete.php?id=$res[Subject_ID]\"
+                        onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+                }
+            ?>
+        </table>
+    </body>
 </html>
